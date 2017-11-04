06b182135369a1f6b6788e2671588e32ea19ef7f
Murat Erhan <murat.erhan.38@gmail.com>
2017-11-04 13:09:04 +0300

    first commit

---
+++ instaCloneApp/feedCell.swift
@@ -0,0 +1,24 @@
+//
+//  feedCell.swift
+//  instaCloneApp
+//
+//  Created by Murat Erhan on 1.11.2017.
+//  Copyright © 2017 Murat Erhan. All rights reserved.
+//
+
+import UIKit
+
+class feedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
